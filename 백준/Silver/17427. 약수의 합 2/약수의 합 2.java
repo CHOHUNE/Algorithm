f13862c8d2666bb31ad86b73b5fc448725fa40d5
HUNE <yuureru@gmail.com>
2024-05-28 01:07:44 +0900

    [Silver II] Title: 약수의 합 2, Time: 136 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main (String[] args)throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int num = Integer.parseInt(br.readLine());
+        long ans=0;
+        
+        for(int i=1; i<=num ; i++){
+            ans+=(long)(num/i)*i;
+        }
+        
+        System.out.println(ans);
+    }
+}
