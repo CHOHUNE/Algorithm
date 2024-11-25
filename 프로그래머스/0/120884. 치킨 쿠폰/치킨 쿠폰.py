85b25d3367ed163550cede24462b01e5a060d4dd
HUNE <yuureru@gmail.com>
2024-11-25 23:15:47 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,10 +1,9 @@
 def solution(chicken):
-    
     ans=0
-    while chicken/10 >= 0.1 :
-            ans+=chicken/10
-            chicken= chicken/10
-    else : return int(ans)
+    while chicken >= 10 :
+        ans+= chicken//10 
+        chicken = chicken//10 + chicken%10
+    else : return ans 
     
     
     
