ebfe4ca376cf99b479c8cc90c4336f6e922e7ec1
HUNE <yuureru@gmail.com>
2024-12-02 13:06:49 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -0,0 +1,13 @@
+def solution ():
+	n= int(input())
+	cnt=0
+	for A in range(0,n+1):
+		for B in range(0,n+1):
+			for C in range(0,n+1):
+				if A+B+C == n:
+					if A>=B+2 :
+						if C%2==0:
+							if A!=0 and B!=0 and C!=0 :
+								cnt+=1
+	print(cnt)
+solution()
