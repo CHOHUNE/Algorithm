395bd5f18c73088f52ec31ccae21b1c6ca032795
HUNE <yuureru@gmail.com>
2024-12-04 11:04:31 +0900

    [Silver III] Title: 수열, Time: 68 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,14 +1,17 @@
 def solution():
-    A,B=map(int,input().split())
+    A,B = map(int,input().split())
     arr = list(map(int,input().split()))
     
-    prefix= [0 for _ in range(A+1)]
+    prefix = [0 for _ in range(A+1)]
     
-
     for i in range(A):
-        prefix[i+1]=prefix[i]+arr[i]
-    maxi =prefix[B]-prefix[0]
+        prefix[i+1]= prefix[i] + arr[i]
+        
+    res =[]
+    
     for k in range(B,A+1):
-        maxi= max(maxi,prefix[k]-prefix[k-B])
-    print(maxi)
+        res.append(prefix[k]-prefix[k-B])
+        
+    print(max(res))
+    
 solution()
