27e2df6a8a25c8ecd73c20e24ee5793e67f62072
HUNE <yuureru@gmail.com>
2024-12-12 15:11:52 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,13 +1,14 @@
-def sol(num):
+def sol(x):
+    global arr
     
-    if num ==0:
-        return 0
-    if num ==1:
-        return 1
-   
-    return sol(num-1)+sol(num-2)
-        
+    if arr[x] != -1:   # 배열의 x번째 값을 검사
+        return arr[x]
+    arr[x] = sol(x-1) + sol(x-2)
+    return arr[x]
 
-    
-num=int(input())
-print(sol(num))
+N = int(input())
+arr = [-1]*(N+2)
+arr[0] = 0
+arr[1] = 1
+
+print(sol(N))
