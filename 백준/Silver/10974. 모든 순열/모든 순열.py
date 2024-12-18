4a76c097313c4edbe863f654840c397aecc6b10a
HUNE <yuureru@gmail.com>
2024-12-18 13:40:38 +0900

    [Silver III] Title: 모든 순열, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,20 +1,6 @@
+from itertools import permutations
 
-N = int(input())
-check = [False]*(N+1)
-choose=[]
+N= int(input())
 
-def sol(level):
-    if level == N:
-        print(' '.join(map(str,choose)))
-        return 
-
-    for i in range(1,N+1):
-        if check[i]==True:
-            continue
-        choose.append(i)
-        check[i]=True
-        sol(level+1)
-        check[i]=False
-        choose.pop()
-    
-sol(0)
+for permutate in permutations(range(1,N+1)):
+    print(' '.join(map(str,permutate)))
