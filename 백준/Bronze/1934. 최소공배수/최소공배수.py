aa57951a9ef02226e11936b60822af6203898101
HUNE <yuureru@gmail.com>
2024-12-22 17:52:43 +0900

    [Bronze I] Title: 최소공배수, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,21 +1,15 @@
-import math
-def get_divisors(n):
-    num=int(math.sqrt(n))
-    ans={1,n}
-    for i in range(2,num+1):
-        if n%i==0:
-            ans.add(i)
-            ans.add(n//i)
-    return ans
-    
+def gcd(a,b):
+
+    if b==0:
+        return a
+    return gcd(b,a%b)
+
 def sol():
-    N=int(input())
+    N= int(input())
     for i in range(N):
-        a,b= map(int,input().split())
-        common_divisors=get_divisors(a)&get_divisors(b)
-        if common_divisors:
-            print((a*b)//max(common_divisors))
-        else:
-            print(a*b)
-sol()
+        a,b = map(int,input().split())
         
+        num = gcd(a,b)
+        print((a*b)//num)
+        
+sol()
