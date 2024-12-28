69337f86f6b90757eb3941f325c4a4b5b02c7e47
HUNE <yuureru@gmail.com>
2024-12-28 20:44:03 +0900

    [Silver III] Title: 분수 합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -0,0 +1,18 @@
+def gcd(a,b):
+    if b==0:
+        return a
+    return gcd(b,a%b)
+
+def sol():
+    a,b=map(int,input().split())
+    c,d=map(int,input().split())
+    
+    N = a*d + b*c 
+    M = b*d
+    
+    GCD=gcd(N,M)
+
+    N = N//GCD
+    M=M//GCD
+    print(N,M)
+sol()
