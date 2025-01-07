01572a5dcf1f59e7d9d04af8e404b0178fe1ace0
HUNE <yuureru@gmail.com>
2025-01-07 22:26:22 +0900

    [level 2] Title: 기능개발, Time: 0.39 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,45 +1,43 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-
+        
+                ArrayList<Integer> ans = new ArrayList<>();
             Queue<Integer> q = new LinkedList<>();
-            ArrayList<Integer> ans = new ArrayList<>();
-            int idx=0;
 
             for (int each : progresses) {
                 q.add(each);
             }
 
+
+            int idx=0;
             while (!q.isEmpty()) {
-                int cnt = 0;
+                
+                int cnt=0;
 
                 for (int i = 0; i < progresses.length; i++) {
                     progresses[i] += speeds[i];
                 }
 
-                while (!q.isEmpty() && progresses[idx] >= 100) {
+                while (!q.isEmpty()&& progresses[idx] >= 100  ) {
                     q.poll();
                     cnt++;
                     idx++;
                 }
-
                 if (cnt > 0) {
                     ans.add(cnt);
-                }
-                
-                }
+                } 
+            }
 
+            int cnt = 0;
             int[] last = new int[ans.size()];
-
-            int cnt=0;
-
             for (int each : ans) {
+
                 last[cnt] = each;
                 cnt++;
             }
 
+
             return last;
-
-            }
-
         }
+    }
