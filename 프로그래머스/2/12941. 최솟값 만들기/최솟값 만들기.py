f1652093608da2c4159269542352bb29b18ba025
HUNE <yuureru@gmail.com>
2025-07-14 21:39:30 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.08 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -2,17 +2,18 @@ import heapq
 
 def solution(A,B):
     
-    heap_a = A.copy()
-    heap_b = [-x for x in B]
+    arr_A= A.copy()
+    arr_B= [ -x for x in B]
     
+    heapq.heapify(arr_A)
+    heapq.heapify(arr_B)
 
-    heapq.heapify(heap_a)
-    heapq.heapify(heap_b)
-    
-    result = 0
-    while heap_a:
-        min_a = heapq.heappop(heap_a)
-        max_b= -heapq.heappop(heap_b)
+    result =0
+    while arr_A:
+        min_a=heapq.heappop(arr_A)
+        max_b=-heapq.heappop(arr_B)
         result += min_a * max_b
+        
+        
 
     return result
