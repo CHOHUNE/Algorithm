58b7549703111cfb77cef9d36f5adde11bf90f6c
HUNE <yuureru@gmail.com>
2025-07-15 21:58:55 +0900

    [level 1] Title: 모의고사, Time: 6.29 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    
+    patterns=[
+        [1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]
+             ]
+    
+    scores=[0,0,0]
+    
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            student_answer = pattern[i % len(pattern)]
+            if student_answer == answer:
+                scores[j] += 1
+    max_score = max(scores)
+    result=[]
+    for i in range(3):
+        if scores[i] == max_score:
+            result.append(i+1)
+            
+
+    return result
