e9ff6fd1356eba9ed0f72e6d942e74e869b5f286
HUNE <yuureru@gmail.com>
2025-07-17 20:11:06 +0900

    [level 1] Title: 모의고사, Time: 6.07 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,18 @@
 def solution(answers):
     patterns=[[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
     score = [0,0,0]
-    
     for i in range(len(answers)):
         answer=answers[i]
         for j in range(len(patterns)):
-            pattern = patterns[j]
+            pattern=patterns[j%i]
+            if answer == pattern:
+                score[j%i] += 1
             
-            if answer == pattern[i%len(pattern)]:
-                score[j]+=1
-    result=[]
-    max_score = max(score)
-    
-    for i in range(len(score)):
-        if max_score == score[i]:
+            
+    result = []
+    max_score =max(score)
+    for i in range(score):
+        if score[i] == max_score:
             result.append(i+1)
-            
+        
     return result
-    
