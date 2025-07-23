ea73f3b16e7f9088018a8e6577f72c41faaf9390
HUNE <yuureru@gmail.com>
2025-07-23 20:08:16 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        
+        arr = array[command[0]-1 :command[1]]
+        arr = sorted(arr)
+        answer.append(arr[command[2]-1])
+
+    return answer
