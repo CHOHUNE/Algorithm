09f022d28176e2d429f377ed0e098ac8e7c40ab1
HUNE <yuureru@gmail.com>
2025-07-23 21:17:56 +0900

    [level 2] Title: 가장 큰 수, Time: 2810.50 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,11 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    answer = ''.join(numbers)
-    return '0' if answer[0] == '0' else answer
+    from functools import cmp_to_key
+    
+    def compare(x, y):
+        if str(x) + str(y) > str(y) + str(x):
+            return -1
+        else : 
+            return 1
+    numbers.sort(key=cmp_to_key(compare))
+    result = ''.join(map(str,numbers))
+    return str(int(result)) # 000 을 0으로 다시 처리하는 부분
