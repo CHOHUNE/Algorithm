916dd61ec13e4cc51f700a9aaedcc32c802c714c
HUNE <yuureru@gmail.com>
2025-07-29 20:55:13 +0900

    [Silver IV] Title: 수들의 합 2, Time: 180 ms, Memory: 110916 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,16 +1,17 @@
 def solution():
-
+    
     N,M = map(int,input().split())
-    num = list(map(int,input().split()))
-
-    ans=0
-    for i in range(N):
-        cnt =0
-        for j in range(i,N):
-            cnt+=num[j]
-            if(cnt == M):
+    num = [0] + list(map(int,input().split()))
+    total = [0] * len(num)
+    ans =0
+    for i in range(len(num)):
+        total[i] = total[i-1] + num[i]
+        
+    for left in range(1,len(num)):
+        for right in range(left,len(num)):
+            if total[right] - total[left-1] == M:
                 ans+=1
-
+                
     print(ans)
 solution()
-
+            
