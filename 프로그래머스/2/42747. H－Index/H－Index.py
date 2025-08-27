dd5f7086c73e3e2a85ab86424598a3d4e7242a98
HUNE <yuureru@gmail.com>
2025-08-27 21:14:43 +0900

    [level 2] Title: H-Index, Time: 41.11 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,14 +1,12 @@
 def solution(citations):
     answer = 0
-    # 0부터 논문 수까지 모든 h 값 체크
-    for h in range(len(citations) + 1):
-        cnt = 0
-        for num in citations:
-            if num >= h:
-                cnt += 1
-        
-        # h번 이상 인용된 논문이 h편 이상이면
-        if cnt >= h:  # 핵심: >= 사용!
-            answer = max(answer, h)  # 최댓값 갱신
     
+    for i in range(len(citations)+1):
+        cnt =0
+        
+        for j in citations:
+            if j >= i:
+                cnt +=1
+        if cnt >= i:
+            answer = max(i,answer)
     return answer
