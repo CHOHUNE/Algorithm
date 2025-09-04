d0d474481f6f6ec599238c0760b055f046e497a2
HUNE <yuureru@gmail.com>
2025-09-04 20:16:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4215.95 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,12 +1,12 @@
 def solution(elements):
     
-    extended = elements * 2 
-    sums = set()
+    twice = elements + elements
+    anset=set()
     
-    for start in range(len(elements)):
-        for length in range(1,len(elements)+1):
-            sub_seq_sum = sum(extended[start: start+length])
-            sums.add(sub_seq_sum)
-        
-    return len(sums)
+    for i in range(len(elements)):
+        for j in range(1,len(elements)+1):
             
+            anset.add(sum(twice[i:i+j]))
+            
+        
+    return len(anset)
