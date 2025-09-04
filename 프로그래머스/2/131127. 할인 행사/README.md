e70c9c021e2deee60c563a22bd5b2b95f1bfae11
HUNE <yuureru@gmail.com>
2025-09-04 20:59:32 +0900

    [level 2] Title: 할인 행사, Time: 104.47 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127)) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 172.32 ms
+메모리: 16.2 MB, 시간: 104.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 25일 23:58:22
+2025년 09월 04일 20:59:30
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,16 +1,18 @@
-from collections import Counter
-
 def solution(want, number, discount):
-    answer = 0
-    want_number = {}
+    compare= []
+    for i in range(len(want)):
+        compare.extend([want[i]]*number[i])
+    compare.sort()
+    # print("comapre",compare)
+    temp=[]
+    cnt=0
     
-    for i, item in enumerate(want) :
-        want_number[item] = number[i]
+    for i in range(0,len(discount)-9):
+        temp=discount[i:i+10]
+        temp.sort()
+        # print("tmep",temp)
+        if temp == compare:
+            cnt+=1
+
         
-    for i in range(len(discount)-9):
-        temp_want_number = Counter(discount[i:i+10])
-        
-        if want_number==temp_want_number:
-            answer += 1 
-            
-    return answer
+    return cnt
