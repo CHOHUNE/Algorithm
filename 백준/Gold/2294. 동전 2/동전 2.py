0519c5cf023c074e65be384fa96f439a31dbb03e
HUNE <yuureru@gmail.com>
2025-09-10 23:28:05 +0900

    [Gold V] Title: 동전 2, Time: 160 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,19 @@
+def solution():
+
+    N, M = map(int,input().split())
+
+    coins = [int(input()) for _ in range(N)]
+
+    dp = [10001] * (M+1)
+    dp[0]=0
+
+    for i in range(N):
+        for j in range(coins[i],M+1):
+
+            dp[j]= min(dp[j],dp[j-coins[i]]+1)
+    
+
+    
+    print(-1 if dp[M] >= 10001 else dp[M])
+
+solution()
