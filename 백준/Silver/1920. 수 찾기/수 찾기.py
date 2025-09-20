ad9b8d392e53ceef4b9abeb29ad2d48bbb230784
HUNE <yuureru@gmail.com>
2025-09-20 16:46:27 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 52432 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,30 +1,16 @@
 import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-def get_idx(arr, n):
-    cur = -1
-    step= len(arr)
-    
-    while step != 0:
-        while cur + step < len(arr) and arr[cur+step] <= n:
-            cur += step
-        step //= 2
-    return cur
+input = sys.stdin.readline
 
 def solution():
     N = int(input())
-    num_list_1 = sorted(list(map(int,input().split())))
+    lis_1 = set(map(int, input().split()))  # set으로 변경
     M = int(input())
-    num_list_2 = list(map(int,input().split()))
+    lis_2 = list(map(int, input().split()))
     
-    for i in num_list_2:
-        idx = get_idx(num_list_1,i)
-        if idx >= 0 and i == num_list_1[idx]:
+    for i in lis_2:
+        if i in lis_1:  # set에서 in은 O(1)
             print(1)
         else:
             print(0)
 
 solution()
-    
-    
-   
