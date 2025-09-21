b35981f277755ae42183045af27c6ac1ce4ebe2b
HUNE <yuureru@gmail.com>
2025-09-21 18:22:49 +0900

    [Silver II] Title: 나무 자르기, Time: 2164 ms, Memory: 145580 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,24 +1,34 @@
-def is_possible(k):
-    
-    ans = 0 
-    for num in num_list:
-        if num > k:
-            ans += num-k
-    return (ans>=M)
+def parametric(trees,mid,M):
+
+    total =0 
+    for tree in trees:
+
+        if tree > mid:
+            total += tree - mid
+
+    return total >= M        
 
 
 def solution():
-    global num_list, M
-    N,M = map(int,input().split())
-    num_list=list(map(int,input().split()))
-    
-    cur = -1 
-    step= int(1e9)+1
-    
-    while step != 0:
-        while (cur+step) < int(1e9) and is_possible(cur+step):
-            cur+=step
-        step //=2
-    print(cur)
-    
+
+    N , M = map(int,input().split())
+    trees = list(map(int,input().split()))
+
+    left = 0            # 아무 것도 안자르는 경우
+    right= max(trees)   # 최대 : 가장 높은 나무
+
+    answer = 0
+
+    while  left <= right:
+        mid = (left + right) // 2
+
+        if parametric(trees, mid, M): # M은 가져야 하는 나무 높이(target)
+            answer = mid 
+            left = mid + 1
+        else :
+            right = mid -1 
+
+    print(answer)
+
 solution()
+
