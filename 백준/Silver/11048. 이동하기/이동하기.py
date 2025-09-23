2d7af6770dc6335a3654fddbe021fcd518fff57f
HUNE <yuureru@gmail.com>
2025-09-23 21:20:32 +0900

    [Silver II] Title: 이동하기, Time: 464 ms, Memory: 80564 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,14 @@
+def solution():
+    
+    N, M = map(int,input().split())
+    
+    lis = [list(map(int,input().split())) for i in range(N)]
+    dp = [[0]*(M+1) for i in range(N+1)]
+    
+    for i in range(1,N+1):
+        for j in range(1,M+1):
+            dp[i][j] = lis[i-1][j-1] + (max(dp[i-1][j],dp[i-1][j-1],dp[i][j-1]))
+
+    print(dp[N][M])
+
+solution()
