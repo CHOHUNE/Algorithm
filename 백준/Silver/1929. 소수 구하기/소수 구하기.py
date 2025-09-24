1919115b9e561ad5f42fe52f785645de4405f8e8
HUNE <yuureru@gmail.com>
2025-09-24 20:59:03 +0900

    [Silver III] Title: 소수 구하기, Time: 156 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -4,7 +4,7 @@ def solution():
     lis = [True] * (M+1)
     lis[0]=lis[1]=False
     
-    for i in range(2,M+1):
+    for i in range(2,int(M**0.5)+1):
         if lis[i]:
             for j in range(i*i,M+1,i):
                 lis[j] = False
@@ -13,3 +13,10 @@ def solution():
             print(i)
 
 solution()
+
+
+
+# 에라토테네스의 체에서 중요한 건 제곱근 까지만 살펴본 후
+# 해당 i 가 소수이면
+# 소수의 배 만큼 False 
+# 전부 True 로 만드는 것도 포인트 
