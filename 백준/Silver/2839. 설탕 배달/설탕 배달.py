7fc6be8aaa035aff205e54a034b1d4982432cc0f
HUNE <yuureru@gmail.com>
2025-10-19 17:58:25 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,13 +1,16 @@
-def sol():
-    num = int(input())
-    arr=[]
-    for three in range((num//3)+1):
-        for five in range((num//5)+1):
-            if 3*three + 5*five ==num:
-                arr.append(five+three)
-    if not arr :
-        print(-1)
-        return
-    print(min(arr))
-sol()
-                
+def solution():
+    N = int(input())
+    cnt = 0 
+    while N >= 0:
+        
+        if N % 5 ==0 :
+            cnt += N//5
+            print(cnt)
+            return
+        N -= 3
+        cnt += 1
+
+
+    print(-1)
+
+solution()
