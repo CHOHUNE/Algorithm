42f909fc049c3aa8baf4e35b32c80e2fb89c74e3
HUNE <yuureru@gmail.com>
2025-11-28 20:39:24 +0900

    [Silver II] Title: 최소 힙, Time: 212 ms, Memory: 41536 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,25 @@
+import sys
+from queue import PriorityQueue
+
+input =sys.stdin.readline
+
+def solution():
+
+    N = int(input())
+
+    lis = PriorityQueue()
+
+    for _ in range(N):
+
+        each =int(input())
+        if each == 0:
+            if not lis.empty():
+                temp = lis.get()
+                print(temp)
+            else:
+                print(0)
+        else:
+            lis.put(each)
+
+
+solution()
