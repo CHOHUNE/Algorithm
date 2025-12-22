1f9b0cd47ba0f76d8d6f57d4bee8c5f717fec629
HUNE <yuureru@gmail.com>
2025-12-22 23:42:47 +0900

    [Silver II] Title: A → B, Time: 88 ms, Memory: 34916 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -9,22 +9,29 @@ def solution():
 
     que = deque([(start,1)])
 
+    ans = -1 
 
-    while que:
-
-        cur = que.popleft()
-        value, idx = cur 
+    def dfs(cur):
+        nonlocal ans
+        #함수 외부에 있는 변수를 변경하려면 nonlocal을 써야한다.
+        #nonlocal 선언 없이 그냥 ans를 쓸 경우 또다른 지역 변수를 만들 뿐이다. 이점 주의 
+        value, idx = cur
 
         if value == end:
-            print(idx)
+            if ans == -1 or idx < ans:
+                ans = idx
             return
 
-        if value > end :
-            continue    
+        elif value > end :
+            return 
 
-        que.append((value*2,idx+1))
-        que.append((int(str(value)+'1'),idx+1))
+        else:
+            dfs((value*2,idx+1))
+            dfs((int(str(value)+'1'),idx+1))
 
-    print(-1)
+
+    dfs((start,1))
+    print(ans)
+    
 
 solution()
