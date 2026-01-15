6777dbce42f4cb717092a0f335351cca7da8e5c9
HUNE <yuureru@gmail.com>
2026-01-15 18:48:38 +0900

    [Silver I] Title: 부등호, Time: 2884 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -10,7 +10,10 @@ def backtrack(N):
     temp = []
     visited = [0] * 10 
 
-    def combinations(N):
+    def combinations(N,NM):
+
+        if len(result) >= 1:
+            return
 
         if len(temp) == N+1:
             for i in range(N):
@@ -24,18 +27,32 @@ def backtrack(N):
 
             result.append(temp[:])
             return
+        if NM == 'MIN':
+            for i in range(10):
 
-        for i in range(10):
+                if visited[i] == 0:
+                    visited[i] = 1
+                    temp.append(i)
+                    combinations(N,'MIN')
+                    temp.pop()
+                    visited[i] = 0
+        if NM =='MAX':
+            for i in range(9,-1,-1):
 
-            if visited[i] == 0:
-                visited[i] = 1
-                temp.append(i)
-                combinations(N)
-                temp.pop()
-                visited[i] = 0
+                if visited[i] == 0:
+                    visited[i] = 1
+                    temp.append(i)
+                    combinations(N,'MAX')
+                    temp.pop()
+                    visited[i] = 0
 
-    combinations(N)
-    print(''.join(map(str,result[-1])))
+
+    
+    combinations(N,'MAX')
+    print(''.join(map(str,result[0])))
+
+    result=[]
+    combinations(N,'MIN')
     print(''.join(map(str,result[0])))
 
 backtrack(N)
